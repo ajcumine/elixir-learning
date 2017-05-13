d0ac5c642a6612cc42c85e5ff0def571493ad9b6
Andrew Cumine <andrewcumine@ajc.local>
2017-05-13 14:13:24 +0100

    messengyr end of building first pages step

--- discover-elixir-and_phoenix/messengyr/web/router.ex
+++ discover-elixir-and_phoenix/messengyr/web/router.ex
@@ -17,6 +17,9 @@ defmodule Messengyr.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+
+    get "/login", PageController, :login
+    get "/signup", PageController, :signup
   end
 
   # Other scopes may use custom stacks.
